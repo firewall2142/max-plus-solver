41af0dadd8d452a7f4fe3c379ca94e25f7f01d23
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-09-03 23:10:53 +0530

    make it a library

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::cmp::{max};
+use std::cmp::{max, min};
 
 #[derive(Copy, Clone)]
 enum R { Pinf, Int(i32), Ninf}
@@ -20,13 +20,29 @@ fn otimes(x: R, y: R) -> R {
         (_, R::Pinf) | (R::Pinf, _) => R::Pinf  // above this
     }
 }
-
 // x⊕y = max(x,y)
 fn oplus(x: R, y: R) -> R {
     match (x, y) {
         (R::Int(a), R::Int(b)) => R::Int(max(a,b)),
         (R::Ninf, z) | (z, R::Ninf) => z,
         (R::Pinf, _) | (_, R::Pinf) => R::Pinf
+    }
+}
+
+// x⊗'y = x + y {Ninf + Pinf = Pinf}
+fn qotimes(x: R, y: R) -> R {
+    match(x, y) {
+        (R::Int(u), R::Int(v)) => R::Int(u+v),
+        (_, R::Pinf) | (R::Pinf, _) => R::Pinf, // this must occur
+        (R::Ninf, _) | (_, R::Ninf) => R::Ninf  // above this
+    }
+}
+// x⊕'y = min(x,y)
+fn qoplus(x: R, y: R) -> R {
+    match (x, y) {
+        (R::Int(a), R::Int(b)) => R::Int(min(a,b)),
+        (R::Pinf, z) | (z, R::Pinf) => z,
+        (R::Ninf, _) | (_, R::Ninf) => R::Ninf
     }
 }
 
